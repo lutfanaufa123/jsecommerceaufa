64831cbe367e151a7f47953f6d3ad02606c82db6
lutfanaufa123 <103198105+lutfanaufa123@users.noreply.github.com>
2022-05-31 22:52:29 +0800

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import Products from './components/Products/Products';
+
+// import Products from './components/Products/Products';
+// import Navbar from './components/Navbar/Navbar';
+// below output is short form to import many items
+import { Products, Navbar } from './components';
 
 const App = () => {
   return (
     <div>
+        <Navbar />
         <Products />
     </div>
-  )
+  );
 }
 
 export default App
